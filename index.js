47186d829fde3ec0c757af88369ba5065346a705
Your Name <munaziribnm@gmail.com>
2022-11-10 13:21:10 +0200

    lkdmld

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const issue2options = {
   credentials: true,
   maxAge: 3600
 };
- app.use(cors({origin: "*",
+ app.use(cors({origin: "http://localhost:3000",
  credentials  : true}));
  
 app.use(Express.urlencoded({ extended: true }))
