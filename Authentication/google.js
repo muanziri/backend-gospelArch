633b8a8ef626403fbd3c23e602456a9d139e9cd0
Your Name <munaziribnm@gmail.com>
2022-11-10 12:51:13 +0200

    changein

--- Authentication/google.js
+++ Authentication/google.js
@@ -46,7 +46,7 @@ var GOOGLE_CLIENT_SECRET='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'
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/auth/google/callback",
+    callbackURL: "https://gospel-archive.herokuapp.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
