97327b8552440b38361b3864da7320678d484d1a
Your Name <munaziribnm@gmail.com>
2022-11-25 11:26:11 +0200

    hello

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const issue2options = {
 };
 app.use(
   cors({
-    origin: ["http://localhost:3001","http://35.197.50.166:3000", "http://localhost:3000"],
+    origin: ["http://localhost:3000","http://35.197.50.166:3000", "http://localhost:3000"],
     methods: ["POST", "GET", "PUSH"],
     credentials: true,
   })
